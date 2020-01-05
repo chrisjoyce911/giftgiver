53f68bd703a880028aaaf7d3f20b33f9aa385c06
Chris Joyce <chrisj@benon.com>
2020-01-06 09:33:48 +1000

    Render the Gift List in the App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-
-// configure({ adapter: new Adapter() });
-
 const app = shallow(<App />);
 
 it('renders correctly', () => {
     expect(app).toMatchSnapshot();
+});
+
+it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift to `state` when clicking the `add-gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
