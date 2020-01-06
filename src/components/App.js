f7d757b070969634b66adcb6273483e0be4deaa8
Chris Joyce <chrisj@benon.com>
2020-01-06 11:51:56 +1000

    Remove a Gift

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,11 @@ class App extends Component {
         this.setState({ gifts });
     }
 
+    removeGift = id => {
+        const gifts = this.state.gifts.filter(gift => gift.id !== id);
+        this.setState({ gifts });
+    }
+
     render() {
         return (
             <div>
@@ -25,7 +30,9 @@ class App extends Component {
                     {
                         this.state.gifts.map(gift => {
                             return (
-                                <Gift key={gift.id} />
+                                <Gift key={gift.id}
+                                    gift={gift}
+                                    removeGift={this.removeGift} />
                             )
                         })
                     }
